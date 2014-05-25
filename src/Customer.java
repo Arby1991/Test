d66f51d9e18739cd5913021264e2c6ee02070135
Arby1991 <arby1991@web.de>
2014-05-25 17:36:22 +0200

    Extracted Method

--- src/Customer.java
+++ src/Customer.java
@@ -24,16 +24,16 @@ class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
+			frequentRenterPoints += each.getFrequentRenterPoints();
 
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
 				frequentRenterPoints++;
 
 			// show figures for the rental
-			result += "/t" + each.getMovie().getTitle() + "/t"
-					+ String.valueOf(each.getCharge()) + "/n";
+			result += "/t" + each.getMovie().getTitle() + "/t" + String.valueOf(each.getCharge()) + "/n";
 			totalAmount +=each.getCharge();
 		}
 		// add footer lines
